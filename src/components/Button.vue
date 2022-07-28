4a8de97c46527bdb223cf0be51a46b66dc3769eb
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-07-28 15:00:22 +0300

    Addes buttons styles

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button v-bind:type="type">
+  <button v-bind:type="type" :class="{ btn: true, 'btn--outlined': outlined }">
     <slot></slot>
   </button>
 </template>
@@ -12,8 +12,29 @@ export default {
       type: String,
       default: "button",
     },
+    outlined: {
+      type: Boolean,
+      default: false,
+    },
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.btn {
+  display: inline-block;
+  font-size: 18px;
+  background: #ff662d;
+  color: #fff;
+  cursor: pointer;
+  min-width: 220px;
+  border: 1px solid transparent;
+  padding: 8px 15px;
+
+  &--outlined {
+    background: none;
+    border: 1px solid #ff662d;
+    color: #ff662d;
+  }
+}
+</style>
