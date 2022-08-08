ab8df88e7c83f19c5c7e6d0b1412ee4374593b6d
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-08-08 13:38:32 +0300

    Creates a container component

---
+++ src/components/Container.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContainerDefault",
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 1210px;
+  margin: 0 auto;
+}
+</style>
