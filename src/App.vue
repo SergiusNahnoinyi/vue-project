0ec47ef5e99a5db6d6b3d7d2d93f43a573429f03
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-08-08 15:34:56 +0300

    Deletes unneccesary code

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,20 @@
 <template>
-  <h1>{{ title }}</h1>
-  <ButtonDefault @click="increment" outlined>Click me</ButtonDefault>
-  <StarRating :rating="4.6" />
   <AppartmentsList :items="appartments" />
 </template>
 
 <script>
-import ButtonDefault from "./components/Button";
-import StarRating from "./components/StarRating";
 import AppartmentsList from "./components/AppartmentsList";
 import appartments from "./components/appartments.js";
 
 export default {
   name: "App",
   components: {
-    ButtonDefault,
-    StarRating,
     AppartmentsList,
   },
   data() {
     return {
-      amountOfClicks: 0,
       appartments,
-      appartment: {
-        id: "5f05c9dad2c9bc0f773444bc",
-        title:
-          "Aut qui adipisci distinctio maiores molestiae sit est inventore vero.",
-        descr:
-          "Non perferendis rerum a in nisi exercitationem dolorum perferendis. Eligendi sit error sed a. Facere maiores sit adipisci sequi eveniet. Qui est voluptatum maiores eos qui vitae.",
-        price: 2032,
-        rating: 4.7,
-        location: {
-          city: "Kherson",
-        },
-        owner: {
-          name: "Sergius",
-          phone: "111-222-3333",
-          email: "sergius@mail.com",
-        },
-      },
     };
-  },
-  computed: {
-    title() {
-      return `Amount of clicks ${this.amountOfClicks}`;
-    },
-  },
-  methods: {
-    increment() {
-      this.amountOfClicks += 1;
-    },
   },
 };
 </script>
