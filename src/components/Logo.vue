10672d077633c535fb731900a757d36989cadeeb
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-08-09 15:08:43 +0300

    Addes an application logo

---
+++ src/components/Logo.vue
@@ -0,0 +1,12 @@
+<template>
+  <img src="../assets/logo.svg" alt="Logo">
+</template>
+
+<script>
+  export default {
+    name: 'LogoComponent'
+  }
+</script>
+
+<style lang="scss" scoped>
+</style> 
