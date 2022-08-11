d264090e4c2dd587730f90c4c46df100077e6912
Sergius Nahnoinyi <86116594+SergiusNahnoinyi@users.noreply.github.com>
2022-08-11 07:53:32 +0300

    Creates a review heading component

---
+++ src/components/ReviewsHeading.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="reviews-heading">
+    <Avatar class="reviews-heading__avatar" />
+    <div class="reviews-heading__content">
+      <span class="reviews-heading__author">{{ author }}</span>
+      <StarRating :rating="rating" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Avatar from './ReviewsAvatar.vue';
+import StarRating from './StarRating.vue';
+export default {
+  name: 'ReviewsHeading',
+  components: {
+    Avatar,
+    StarRating,
+  },
+  props: {
+    author: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.reviews-heading {
+  display: flex;
+  &__avatar {
+    margin-right: 17px;
+  }
+  &__content {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+  }
+}
+</style> 
