55095abc65a999979247dfd47b27e4110794e974
SergiusNahnoinyi <sergiusnahnoinyi@gmail.com>
2022-08-20 07:43:59 +0300

    Installs vuex and sets up a store

---
+++ src/vuex/store.js
@@ -0,0 +1,37 @@
+import { createStore } from "vuex";
+import { loginUser, registerUser } from "../services/appartmentsService";
+
+const initialState = {
+  user: null,
+  token: "",
+};
+
+const store = createStore({
+  state: { ...initialState },
+  mutations: {
+    setUserData(state, userData) {
+      state.user = userData;
+    },
+    setToken(state, token) {
+      state.token = token;
+    },
+  },
+  actions: {
+    async login({ commit }, payload) {
+      const { data } = await loginUser(payload);
+      const { user, token } = data;
+
+      commit("setUserData", user);
+      commit("setToken", token);
+    },
+    async registration({ commit }, payload) {
+      const { data } = await registerUser(payload);
+      const { user, token } = data;
+
+      commit("setUserData", user);
+      commit("setToken", token);
+    },
+  },
+});
+
+export default store;
